b6bb04aa9e573912072ad536ca5d95068fe1d70b
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-10-01 12:12:07 +0530

    fixed user middleware

--- server/middlewares/user.js
+++ server/middlewares/user.js
@@ -3,13 +3,21 @@ const JWT_USER_PASSWORD = require("../config")
 
  function userAuthMiddleware(req,res,next){
     const token = req.headers.token;
-    const decoded = jwt.verify(token,JWT_USER_PASSWORD);
-    if(decoded){
-        req.userId = decoded.id;
-        next();
-    }else{
-        return res.status(404).json({
-            message:"Not authenticated"
+    try{
+        const decoded = jwt.verify(token,JWT_USER_PASSWORD);
+        if(decoded){
+            req.userId = decoded.id;
+            next();
+        }else{
+            return res.status(404).json({
+                message:"Not authenticated"
+            })
+        }
+    }catch(err){
+        console.log(err);
+        
+        res.status(403).json({
+            message:"something went wrong"
         })
     }
 }
